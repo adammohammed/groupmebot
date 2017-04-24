d5de5fa32f905db5a1ec3513312530c32d13608a
adammohammed <aqm5498@psu.edu>
2017-04-24 18:30:46 -0400

    Placed GroupmeBot in its own package so it can be used elsewhere

---
+++ groupmebot.go
@@ -0,0 +1,110 @@
+package groupmebot
+
+import (
+	"encoding/json"
+	"io"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strings"
+)
+
+type GroupMeBot struct {
+	ID      string `json:"bot_id"`
+	GroupID string `json:"group_id"`
+	Host    string `json:"host"`
+	Port    string `json:"port"`
+	Server  string
+	Hooks   []func(InboundMessage) (bool, string)
+}
+
+type InboundMessage struct {
+	Avatar_url  string `json:"avatar_url"`
+	Id          string `json:"id"`
+	Name        string `json:"name"`
+	Sender_id   string `json:"sender_id"`
+	Sender_type string `json:"sender_type"`
+	System      bool   `json:"system"`
+	Text        string `json:"text"`
+	User_id     string `json:"user_id"`
+}
+
+type OutboundMessage struct {
+	ID   string `json:"bot_id"`
+	Text string `json:"text"`
+}
+
+/// NewBotFromJson (json cfg file name)
+/// This reads a json file containing the keys
+/// See the example bot_cfg.json
+/// Returns err from ioutil if file can not be read
+func NewBotFromJson(filename string) (*GroupMeBot, error) {
+	file, err := ioutil.ReadFile(filename)
+
+	var bot GroupMeBot
+	if err != nil {
+		log.Fatal("Error reading bot configuration json file")
+		return nil, err
+	}
+
+	// Parse out information from file
+	json.Unmarshal(file, &bot)
+
+	bot.Server = bot.Host + ":" + bot.Port
+
+	bot.Hooks = make([]func(InboundMessage) (bool, string), 0, 10)
+
+	return &bot, err
+}
+
+func (b *GroupMeBot) SendMessage(outMessage string) (*http.Response, error) {
+	msg := OutboundMessage{b.ID, outMessage}
+	payload, err := json.Marshal(msg)
+	if err != nil {
+		return nil, err
+	}
+
+	j_payload := string(payload)
+	return http.Post("https://api.groupme.com/v3/bots/post", "application/json", strings.NewReader(j_payload))
+}
+
+/*
+ This is legitimate black magic, this is pretty cool, not usually able to do
+ things like this in other languages. This is a function that takes
+ a list of trigger functions and returns a function that can handle the Server
+ Requests
+*/
+func (b *GroupMeBot) Handler() http.HandlerFunc {
+	// Request Handler function
+	numhooks := len(b.Hooks)
+
+	return func(w http.ResponseWriter, req *http.Request) {
+		if req.Method == "POST" {
+			log.Println("Bot recieving and handling message.")
+			defer req.Body.Close()
+			var msg InboundMessage
+			err := json.NewDecoder(req.Body).Decode(&msg)
+
+			// Find hook by running through hooklist
+			hook, resp := false, ""
+			for i := 0; !hook && i < numhooks; i++ {
+				hook, resp = b.Hooks[i](msg)
+			}
+
+			if hook {
+				log.Printf("Sending message: %v\n", resp)
+				_, err := b.SendMessage(resp)
+				if err != nil {
+					log.Fatal("Error when sending.", err)
+				}
+			}
+
+			if err != nil {
+				log.Fatal("Couldn't read all the body", err)
+			}
+		} else {
+			log.Println("Bot not responding to unknown message")
+			io.WriteString(w, "GOTEM")
+		}
+	}
+}
